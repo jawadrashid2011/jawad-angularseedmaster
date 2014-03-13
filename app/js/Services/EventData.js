75588dcba6bebcf49a9394276c8ee7bf506aa144
Jawad Rashid <jawadrashid2010@gmail.com>
2014-03-13 15:03:55 +0500

    Before Promise work

--- app/js/Services/EventData.js
+++ app/js/Services/EventData.js
@@ -2,45 +2,19 @@
 /* 
  * Created by Jawad Rashid
  */
-eventsApp.factory("eventData", function() {
+eventsApp.factory("eventData", function($http, $log) {
     return {
-        event: {
-            name: "Learning with Jawad",
-            date: "12/2/2014",
-            time: "3 PM",
-            location: {
-                address: "S-Block",
-                city: "Lahore",
-                province: "Punjab"
-            },
-            imageUrl: "img/angularjs-logo.png",
-            sessions: [
-                {
-                    name: "Learning Angular Fundamentals",
-                    creatorName: "Jawad Rashid",
-                    duration: 4,
-                    level: "Noob",
-                    abstract: "This is the main angular learning fundamentals course",
-                    upVoteCount: 0
-                },
-                {
-                    name: "Angular with MEAN stack",
-                    creatorName: "Asad Aijaz",
-                    duration: 6,
-                    level: "Intermediate",
-                    abstract: "A interesting course with MongoDB, Angular, Express, Node, and express.js",
-                    upVoteCount: 0
-                },
-                {
-                    name: "Learn CodeIgnitor",
-                    creatorName: "Jawad Rashid",
-                    duration: 1,
-                    level: "Expert",
-                    abstract: "Learning everything about the codeigniter.",
-                    upVoteCount: 0
-                }
-            ]
+        getEvent: function(successcb) {
+            $http({
+                method: 'GET',
+                url: 'data/event/1.json'
+            }).
+            success(function(data, status, headers, config) {
+                $log.info(data, status, headers(), config);
+                successcb(data);
+            }).error(function(data, status, headers, config) {
+                $log.warn(data, status, headers(), config);
+            });
         }
     };
 });
-
--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,24 +2,26 @@
 
 eventsApp.controller("EventsController",
     function EventsController($scope, eventData) {
-        
-        $scope.event = eventData.event;
-        
+
+        eventData.getEvent(function(event) {
+            $scope.event = event;
+        });
+
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
         };
-        
+
         $scope.downVoteSession = function(session) {
             session.upVoteCount--;
         };
-        
+
         $scope.snippet = "<span style='color:red;'>Hi there</span>";
         $scope.boolValue = true;
-        $scope.mystyle = {color:'red'};
+        $scope.mystyle = {
+            color: 'red'
+        };
         $scope.myclass = "blue";
         $scope.buttonDisabled = true;
         $scope.sortorder = "name";
     }
 );
-
-
