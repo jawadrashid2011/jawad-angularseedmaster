3245713d8dcf030b732c8cbc47328764f556d4e2
Jawad Rashid <jawadrashid2010@gmail.com>
2014-03-29 16:10:49 +0500

    Using compile to repeat

---
+++ app/js/directives/repeatx.js
@@ -0,0 +1,24 @@
+'use strict';
+
+eventsApp.directive('repeatX', function( /*$compile*/ ) {
+    return {
+        restrict: 'A',
+        // link: function(scope, element, attrs, controller) {
+        //     for (var i = 0; i < Number(attrs.repeatX) - 1; i++) {
+        //         element.after($compile(element.clone().attr("repeat-x", 0))(scope));
+        //     }
+        // }
+        compile: function(element, attrs) {
+            for (var i = 0; i < Number(attrs.repeatX) - 1; i++) {
+                element.after(element.clone().attr("repeat-x", 0));
+            }
+            return function(scope, element, attributes, controller) {
+                attributes.$observe('text', function(newValue) {
+                    if (newValue == 'Hello World') {
+                        element.css('color', 'red');
+                    }
+                });
+            }
+        }
+    }
+});
