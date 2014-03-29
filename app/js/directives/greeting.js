248c8b724f7ffb05109b13ebae3b93b03639eba5
Jawad Rashid <jawadrashid2010@gmail.com>
2014-03-29 15:00:29 +0500

    Using Controller within directives

---
+++ app/js/directives/greeting.js
@@ -0,0 +1,25 @@
+'use strict';
+
+eventsApp.directive('greeting', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: "<button class='btn' ng-click='sayHello()' >Say Hello</button>",
+        // controller: function($scope) {
+        //     $scope.sayHello = function() {
+        //         alert('Hello');
+        //     }
+        // }
+        // controller: "GreetingController"
+        controller: "@",
+        name: 'ctrl'
+    }
+});
+
+eventsApp.controller('GreetingController',
+    function GreetingController($scope) {
+        $scope.sayHello = function() {
+            alert('Hello Jawad');
+        }
+    }
+);
